f73e38c0b049e4f1eed1e445cff1ddf7a551136d
Andrew Milson <andrew.milson@nyriad.com>
2015-12-16 22:19:16 +1300

    General

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 About
 =====
+
 Recieves webhooks from Dockerhub to rebuild docker containers on the host with the latest image from Dockerhub.
 
 Dependencies
 ============
+
 + Node >= 0.12
 
 Install Instructions
 ====================
+
 ```
 sudo npm install -g webhooker
 webhooker --config config.json
--- app.js
+++ app.js
@@ -1,26 +1,21 @@
-const koa = require('koa');
-const exec = require('child_process').exec;
-const app = koa();
-const configFile = require(process.argv[
+#!/usr/bin/env node
+
+var express = require('express')
+var app = express()
+var configFile = require(process.argv[
   process.argv.indexOf('--config') + 1]);
 
-const routes = new require('koa-router')();
-
 configFile.forEach(function(config) {
-  routes.post('/hooks/' + config.id, function *() {
-    exec(config[execute-command], (err, stdout, stderr) => {
+  app.get('/hooks/' + config.id, function(req, res) {
+    console.log("YOW")
+    exec(config['execute-command'], (err, stdout, stderr) => {
       if (err) throw err;
       console.log(stdout, stderr);
     });
-  });
+  })
 });
 
-app.use(routes.middleware());
-const server = app.listen(9000);
+app.listen(9909)
 console.log("Listening for webhooks on:" +
   configFile.map(config => '\n + ' +
   'http://localhost:9000/hooks/' + config.id))
-
-npm set init.author.name "Andrew Milson"
-npm set init.author.email "andrew.j.milson@gmail.com"
-npm set init.author.url "http://andrewmilson.com"
