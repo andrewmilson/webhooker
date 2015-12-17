054de520475efcd676ed6ad2c88f6000e7aff225
Andrew Milson <andrew.milson@gmail.com>
2015-12-17 19:58:17 +1300

    Fix exec issue

--- app.js
+++ app.js
@@ -9,13 +9,14 @@ var port = ~portIndex ? process.argv[portIndex + 1] : 9000;
 
 var path = require("path");
 var configPath = path.join(process.cwd(), process.argv[
-  process.argv.indexOf('--config') + 1])
+  process.argv.indexOf('--config') + 1]);
+var configDir = path.dirname(configPath);
 var configFile = require(configPath);
 
 configFile.forEach(function(config) {
   app.post('/hooks/' + config.id, function(req, res) {
     console.log("Received webhook:", config.id)
-    exec.execFile(config.cmd, {cwd: path.dirname(configPath)},
+    exec.exec(config.cmd, [], {cwd: configDir},
     function(err, stdout, stderr) {
       if (err) throw err;
       console.log(stdout, stderr);
